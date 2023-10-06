50065c58e2d5a786d93aa768cda59f87c0e7e633
periiProggramerGuy <jonathan.peri56@gmail.com>
2023-10-07 00:03:37 +0300

    v07/10/2023

--- Client/CommandClient.py
+++ Client/CommandClient.py
@@ -1,30 +1,35 @@
 import socket
+import subprocess
+import threading
+
 def RunClient(ip):
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    server_address = (ip, 12345) 
+    server_address = (ip, 12345)
 
     client_socket.connect(server_address)
 
+    def receive_responses():
+        while True:
+            response = client_socket.recv(1024).decode()
+            print("Server Response:")
+            print(response)
+
+    response_thread = threading.Thread(target=receive_responses)
+    response_thread.start()
+
     try:
         while True:
-            
             command = input("Enter a command to send to the server (or 'exit' to quit): ")
 
             if command.lower() == 'exit':
+                client_socket.send(("command:exit").encode())  # Send an exit command to the server
                 break
 
             if command.strip():
-                
                 client_socket.send(("command:" + command).encode())
-
-                
-                response = client_socket.recv(1024).decode()
-                print("Server Response:")
-                print(response)
 
     except Exception as e:
         print(f"Error: {e}")
 
-    
     client_socket.close()
